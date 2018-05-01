828993e5a44e30d145691f4d384fd3b86d66e0c3
vitaliibalakin <balakinvitalyv@gmail.com>
2018-05-01 10:00:23 +0700

    new dialog window control

--- plot_diss.py
+++ plot_diss.py
@@ -16,9 +16,9 @@ class PlotDissectorData(QtGui.QMainWindow):
 
         self.init_chans(devname)
 
-        self.setWindowTitle("Plot")
+        self.setWindowTitle("Signal from dissector")
         self.plot_window = pg.GraphicsLayoutWidget(parent=self)
-        self.diss_plot = self.plot_window.addPlot(title='Signal from dissector', enableMenu=False)
+        self.diss_plot = self.plot_window.addPlot(enableMenu=False)
         self.diss_plot.showGrid(x=True, y=True)
         self.diss_plot.setLabel('left', "Voltage", units='mV')
         self.diss_plot.setLabel('bottom', "Time", units='Ns')
@@ -30,13 +30,12 @@ class PlotDissectorData(QtGui.QMainWindow):
 
         self.btn_save.clicked.connect(self.act_save)
         self.btn_settings.clicked.connect(self.act_settings)
-        #self.actionSettings.triggered.connect(self.act_set)
 
     def act_save(self):
         self.dial_save = save_dial.DialSave()
 
     def act_settings(self):
-        self.dial_settings = settings_dial.DialSet()
+        self.dial_set = settings_dial.DialSet(self.chan_light, self.chan_phase, self.chan_ampl)
 
     def plot_(self, chan):
         new_size = 2150
@@ -57,46 +56,18 @@ class PlotDissectorData(QtGui.QMainWindow):
             self.diss_plot.plot(x_fit_data, y_fit_data, pen=pg.mkPen('r', width=5))
 
     def init_chans(self, devname):
-        self.chan_ampl = cda.DChan("cxhw:18.diss208.out1")      #these 3 for diss control
-        self.chan_phase = cda.DChan("cxhw:18.diss208.out0")
-        self.chan_light = cda.DChan("cxhw:18.diss208.outrb0")
-
         self.chan_fit_data = cda.VChan(devname + ".fit_data", max_nelems=65535)
         self.chan_thinned_data = cda.VChan(devname + ".resample_data", max_nelems=65535)
         self.chan_time_fit_data = cda.VChan(devname + ".time_fit_data", max_nelems=65535)
         self.chan_sigma = cda.DChan(devname + ".fit_sigma", on_update=1)
         self.chan_amplitude = cda.DChan(devname + ".fit_a")
         self.chan_t0 = cda.DChan(devname + ".fit_t0")
-
-        self.chan_phase.valueMeasured.connect(self.init_val)        #these 6 for diss control
-        self.chan_ampl.valueMeasured.connect(self.init_val)
-        self.chan_light.valueMeasured.connect(self.init_val)
-        self.phase_val.valueChanged.connect(self.renewal)
-        self.ampl_val.valueChanged.connect(self.renewal)
-        self.light.toggled.connect(self.light_switch)
-
         self.chan_sigma.valueMeasured.connect(self.plot_)
 
-    def light_switch(self):                                     #for diss control
-        if self.light.isChecked():
-            self.chan_light.setValue(1)
-            self.light.setText("Light ON")
-        else:
-            self.chan_light.setValue(0)
-            self.light.setText("Light OFF")
+        self.chan_ampl = cda.DChan("cxhw:18.diss208.out1")
+        self.chan_phase = cda.DChan("cxhw:18.diss208.out0")
+        self.chan_light = cda.DChan("cxhw:18.diss208.outrb0")
 
-    def renewal(self):                                          #for diss control
-        self.chan_phase.setValue(self.phase_val.value())
-        self.chan_ampl.setValue(self.ampl_val.value())
-
-    def init_val(self):                                         #for diss control
-        self.phase_val.setValue(self.chan_phase.val)
-        self.ampl_val.setValue(self.chan_ampl.val)
-        self.light.setChecked(self.chan_light.val)
-        if self.chan_light.val:
-            self.light.setText("Light ON")
-        else:
-            self.light.setText("Light OFF")
 
 app = QtGui.QApplication(['plot'])
 w = PlotDissectorData('cxhw:0.e_diss')
--- settings_dial.py
+++ settings_dial.py
@@ -2,32 +2,39 @@ from PyQt4 import QtGui, uic
 import sys
 import pycx4.qcda as cda
 
+
 class DialSet(QtGui.QDialog):
-    def __init__(self):
+    def __init__(self, chan_light, chan_phase, chan_ampl):
         super(DialSet, self).__init__()
         uic.loadUi("set_dial.ui", self)
 
-        self.chan_ampl = cda.DChan("cxhw:18.diss208.out1")
-        self.chan_phase = cda.DChan("cxhw:18.diss208.out0")
-        self.chan_light = cda.DChan("cxhw:18.diss208.outrb0")
+        # self.chan_ampl = cda.DChan("cxhw:18.diss208.out1")
+        # self.chan_phase = cda.DChan("cxhw:18.diss208.out0")
+        # self.chan_light = cda.DChan("cxhw:18.diss208.outrb0")
 
-        self.show()
+        self.chan_light = chan_light
+        self.chan_phase = chan_phase
+        self.chan_ampl = chan_ampl
 
-        self.chan_phase.valueMeasured.connect(self.init_val)
-        self.chan_ampl.valueMeasured.connect(self.init_val)
-        self.chan_light.valueMeasured.connect(self.init_val)
+        self.init_val()
+
+        # chan_phase.valueMeasured.connect(self.init_val)
+        # chan_ampl.valueMeasured.connect(self.init_val)
+        # chan_light.valueMeasured.connect(self.init_val)
 
         self.phase_val.valueChanged.connect(self.renewal)
         self.ampl_val.valueChanged.connect(self.renewal)
         self.light.toggled.connect(self.light_switch)
 
+        self.show()
+
     def light_switch(self):
         if self.light.isChecked():
             self.chan_light.setValue(1)
-            self.label_light.setText("Light ON")
+            self.light.setText("Light ON")
         else:
             self.chan_light.setValue(0)
-            self.label_light.setText("Light OFF")
+            self.light.setText("Light OFF")
 
     def renewal(self):
         self.chan_phase.setValue(self.phase_val.value())
@@ -38,12 +45,15 @@ class DialSet(QtGui.QDialog):
         self.ampl_val.setValue(self.chan_ampl.val)
         self.light.setChecked(self.chan_light.val)
         if self.chan_light.val:
-            self.label_light.setText("Light ON")
+            self.light.setText("Light ON")
         else:
-            self.label_light.setText("Light OFF")
+            self.light.setText("Light OFF")
 
 
 if __name__ == "__main__":
     app = QtGui.QApplication(['set'])
-    w = DialSet()
+    chan_ampl = cda.DChan("cxhw:18.diss208.out1")
+    chan_phase = cda.DChan("cxhw:18.diss208.out0")
+    chan_light = cda.DChan("cxhw:18.diss208.outrb0")
+    w = DialSet(chan_light, chan_phase, chan_ampl)
     sys.exit(app.exec_())
