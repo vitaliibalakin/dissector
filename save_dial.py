f652e4c0dcd18f65ff218f09e54435b1583cde33
vitaliibalakin <balakinvitalyv@gmail.com>
2018-06-21 21:30:17 +0700

    new beam size count & peak position

--- save_dial.py
+++ save_dial.py
@@ -11,11 +11,6 @@ class DialSave(QtGui.QDialog):
         super(DialSave, self).__init__()
         uic.loadUi("save_dial.ui", self)
 
-        # self.chan_sigma = cda.DChan("cxhw:0.e_diss.fit_sigma")
-        # self.chan_cur = cda.DChan("cxhw:0.dcct.beamcurrent")
-        # self.chan_t0 = cda.DChan("cxhw:0.e_diss.fit_t0")
-        # self.chan_accuracy = cda.DChan("cxhw:0.e_diss.fit_a")
-
         self.chan_sigma = chan_sigma
         self.chan_cur = chan_cur
         self.chan_t0 = chan_t0
@@ -28,46 +23,69 @@ class DialSave(QtGui.QDialog):
         self.show()
 
         self.connect(self.btn_save, QtCore.SIGNAL("clicked()"), self.push_btn_save)
-        self.connect(self.btn_begin_meas, QtCore.SIGNAL("clicked()"), self.begin_meas)
-        self.connect(self.btn_finish_meas, QtCore.SIGNAL("clicked()"), self.finish_meas)
 
         self.av_num = 0
+        self.sv_open = 0
 
         self.cur_data = []
         self.sigma_data = []
         self.accuracy = []
         self.counter = 0
         self.chans = {}
+        self.wr_data = []
+        self.file_name = ''
+        self.osc_file_name = ''
 
     def push_btn_save(self):
-        if self.ln_filename.text() == 'Write the name of file':
-            self.lbl_status.setText("No filename")
+        if not self.sv_open:
+            if self.ln_filename.text() != 'Name of file':
+                self.sv_open = 1
+                self.av_num = self.box_av_num.value()
+
+                date = datetime.datetime.now().strftime('%Y-%m-%d')
+                self.file_name = self.ln_filename.text() + '_' + date + '.dat'
+                if self.av_num == 1:
+                    self.osc_file_name = self.ln_filename.text() + '_' + 'osc' + '_' + date + '.dat'
+
+                self.wr_data = np.zeros((3, 1), dtype=np.double)
+                self.chans['sigma'] = np.zeros((self.av_num,), dtype=np.double)
+                self.chans['beam_current'] = np.zeros((self.av_num,), dtype=np.double)
+                self.chans['t_0'] = np.zeros((self.av_num,), dtype=np.double)
+
+                self.lbl_status.setText("Saving")
+                self.counter = self.av_num
+                self.chan_make_model_fit.setValue(1)
+            else:
+                self.lbl_status.setText("Write namefile")
         else:
             self.lbl_status.setText("Saving")
             self.counter = self.av_num
             self.chan_make_model_fit.setValue(1)
 
     def new_val_cb(self):
-        if self.counter != 0:
-            print self.counter
-            self.chans['sigma'][self.counter - 1] = self.chan_sigma.val
-            self.chans['beam_current'][self.counter - 1] = self.chan_cur.val
-            self.chans['t_0'][self.counter - 1] = self.chan_t0.val
+        if self.counter:
+            counter = self.counter
             self.counter -= 1
+            print counter
+            self.chans['sigma'][counter - 1] = self.chan_sigma.val
+            self.chans['beam_current'][counter - 1] = self.chan_cur.val
+            self.chans['t_0'][counter - 1] = self.chan_t0.val
             self.chan_make_model_fit.setValue(1)
-            if self.counter:
-                pass
-            else:
+            if not self.counter:
+                f = open(self.file_name, 'a')
                 self.wr_data[0] = np.mean(self.chans['beam_current'])
                 self.wr_data[1] = np.mean(self.chans['sigma'])
                 self.wr_data[2] = np.mean(self.chans['t_0'])
-                np.savetxt(self.f, self.wr_data.T, delimiter=' ', newline='\n')
+                np.savetxt(f, self.wr_data.T, delimiter=' ', newline='\n')
+                f.close()
                 if self.av_num == 1:
+                    f_osc = open(self.osc_file_name, 'a')
                     x_data = self.chan_time_fit_data.val.tolist()
                     y_data = self.chan_fit_data.val.tolist()
                     osc_data = {'current': self.chans['beam_current'][0], 'x_data': x_data, 'y_data': y_data}
-                    self.f_osc.write(json.dumps(osc_data))
-                    self.f_osc.write('\n')
+                    f_osc.write(json.dumps(osc_data))
+                    f_osc.write('\n')
+                    f_osc.close()
                 print self.wr_data.T
                 self.lbl_status.setText("The value was saved")
         else:
@@ -75,24 +93,12 @@ class DialSave(QtGui.QDialog):
 
     def begin_meas(self):
         if self.ln_filename.text() != 'Write the name of file':
-            date = datetime.datetime.now().strftime('%Y-%m-%d')
-            self.f = open(self.ln_filename.text() + '_' + date + '.dat', 'w')
-            self.av_num = self.box_av_num.value()
-            self.wr_data = np.zeros((3, 1), dtype=np.double)
-            self.chans['sigma'] = np.zeros((self.av_num,), dtype=np.double)
-            self.chans['beam_current'] = np.zeros((self.av_num,), dtype=np.double)
-            self.chans['t_0'] = np.zeros((self.av_num,), dtype=np.double)
+
             if self.av_num == 1:
                 self.f_osc = open(self.ln_filename.text() + '_' + 'osc' + '_' + date + '.dat', 'w')
             self.lbl_status.setText('Meas began')
         else:
             self.lbl_status.setText('Write the filename')
-
-    def finish_meas(self):
-        self.f.close()
-        if self.av_num == 1:
-            self.f_osc.close()
-        self.lbl_status.setText('Meas finished')
 
 
 if __name__ == "__main__":
